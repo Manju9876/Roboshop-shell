6093403a2ad8e4ac7d1b864cf353f2760ee23d68
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2023-04-19 17:24:35 +0530

    mongodb code (required basic script) code 1

--- catalogue.sh
+++ catalogue.sh
@@ -1,16 +1,31 @@
+echo -e "\e[32m>>>>>>>>>>>>>> configuration of node js<<<<<<<<<<<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+echo -e "\e[32m>>>>>>>>>>>>>> install of node js<<<<<<<<<<<<<<<<<<<\e[0m"
 yum install nodejs -y
+echo -e "\e[32m>>>>>>>>>>>>>> starting and enabling node js <<<<<<<<<<<<<<<<<<<\e[0m"
 systemctl start catalogue
 systemctl enable catalogue
+echo -e "\e[32m>>>>>>>>>>>>>> creating a user <<<<<<<<<<<<<<<<<<<\e[0m"
 useradd roboshop
+echo -e "\e[32m>>>>>>>>>>>>>> creating a directory <<<<<<<<<<<<<<<<<<<\e[0m"
 mkdir /app
+echo -e "\e[32m>>>>>>>>>>>>>> dowloading code<<<<<<<<<<<<<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+echo -e "\e[32m>>>>>>>>>>>>>> changed directory <<<<<<<<<<<<<<<<<<<\e[0m"
 cd /app
+echo -e "\e[32m>>>>>>>>>>>>>> unziped the content we downloaded <<<<<<<<<<<<<<<<<<<\e[0m"
 unzip /tmp/catalogue.zip
+echo -e "\e[32m>>>>>>>>>>>>>> installing the dependencies<<<<<<<<<<<<<<<<<<<\e[0m"
 npm install
+echo -e "\e[32m>>>>>>>>>>>>>> copying the configuration file to the systemd <<<<<<<<<<<<<<<<<<<\e[0m"
 cp /home/centos/roboshop-shell/catalogue.service  /etc/systemd/system/catalogue.service
+echo -e "\e[32m>>>>>>>>>>>>>> reloading the service in systemd <<<<<<<<<<<<<<<<<<<\e[0m"
 systemctl daemon-reload
+echo -e "\e[32m>>>>>>>>>>>>>> restarting the service in systemd <<<<<<<<<<<<<<<<<<<\e[0m"
 systemctl restart catalogue
+echo -e "\e[32m>>>>>>>>>>>>>> copying the mongodb repo file <<<<<<<<<<<<<<<<<<<\e[0m"
 cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[32m>>>>>>>>>>>>>> installing the mongodb client <<<<<<<<<<<<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
+echo -e "\e[32m>>>>>>>>>>>>>> loading the schema <<<<<<<<<<<<<<<<<<<\e[0m"
 mongo --host mongodb-dev.manju-devops.online </app/schema/catalogue.js
