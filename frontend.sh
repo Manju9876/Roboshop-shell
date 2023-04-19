898a9c142de0f87ceddf4047661db132f8e9d1f9
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2023-04-19 18:05:56 +0530

    mongodb code (required basic script) code 1

--- frontend.sh
+++ frontend.sh
@@ -1,18 +1,25 @@
 echo -e "\e[31m >>>>>>>>>>>>>>>>>installing nginx<<<<<<<<<<<<<<<<<<<<\e[0m"
 yum install nginx -y
+
 echo -e "\e[31m>>>>>>>>>>>>>> starting and enabling the nginx service <<<<<<<<<<<<<<<<<<<\e[0m"
 systemctl enable nginx
 systemctl start nginx
+
 echo -e "\e[31m >>>>>>>>>>>>>>>>>remving the existing content in nginx<<<<<<<<<<<<<<<<<<<<\e[0m"
 rm -rf /usr/share/nginx/html/*
+
 echo -e "\e[31m >>>>>>>>>>>>>>>>>downloadig the  application code <<<<<<<<<<<<<<<<<<<<\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
 echo -e "\e[31m >>>>>>>>>>>>>>>>>changing the directory and unzip the downloaded code <<<<<<<<<<<<<<<<<<<<\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+
 echo -e "\e[31m >>>>>>>>>>>>>>>>>copying the congiguration file<<<<<<<<<<<<<<<<<<<<\e[0m"
 cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
+
 echo -e "\e[31m >>>>>>>>>>>>>>>>>restarting the nginx<<<<<<<<<<<<<<<<<<<<\e[0m"
 systemctl restart nginx
+
 echo -e "\e[31m >>>>>>>>>>>>>>>>>checking the status of nginx service <<<<<<<<<<<<<<<<<<<<\e[0m"
 systemctl status nginx
---
+++ redis.sh
@@ -0,0 +1,23 @@
+
+echo -e "\e[32m>>>>>>>>>>>>>>>>>>>>>>>>>>> downloading repo file for redis <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+
+echo -e "\e[32m>>>>>>>>>>>>>>>>>>>>>>>>>>> enabling the redis 6.2 version <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+dnf module enable redis:remi-6.2 -y
+
+echo -e "\e[32m>>>>>>>>>>>>>>>>>>>>>>>>>>> installing the redis software <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+yum install redis -y
+
+echo -e "\e[32m>>>>>>>>>>>>>>>>>>>>>>>>>>> replacing the IP address to listen to 0.0.0.0 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+sed -i -re 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
+
+echo -e "\e[32m>>>>>>>>>>>>>>>>>>>>>>>>>>> starting and enabling the redis service <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+systemctl enable redis
+systemctl start redis
+
+echo -e "\e[32m>>>>>>>>>>>>>>>>>>>>>>>>>>> restart redis and check the status of redis <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+systemctl restart redis
+systemctl status redis
+
+echo -e "\e[32m>>>>>>>>>>>>>>>>>>>>>>>>>>> check the server of redis is listen to 'all'  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+netstat -lntp
