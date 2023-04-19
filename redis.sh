a28b3d386e75a1de0673b189c8f401b4cd92da28
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2023-04-19 19:11:11 +0530

    mongodb code (required basic script) code 1

--- redis.sh
+++ redis.sh
@@ -9,7 +9,7 @@ echo -e "\e[32m>>>>>>>>>>>>>>>>>>>>>>>>>>> installing the redis software <<<<<<<
 yum install redis -y
 
 echo -e "\e[32m>>>>>>>>>>>>>>>>>>>>>>>>>>> replacing the IP address to listen to 0.0.0.0 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-sed -i -re 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
 
 echo -e "\e[32m>>>>>>>>>>>>>>>>>>>>>>>>>>> starting and enabling the redis service <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 systemctl enable redis
