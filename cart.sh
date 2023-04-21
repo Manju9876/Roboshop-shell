801259647bb496d4a3dea7f8c18c7be26ceea93e
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2023-04-21 18:13:41 +0530

    source common code

--- cart.sh
+++ cart.sh
@@ -3,9 +3,7 @@ script_path=$(dirname "script")
 source ${script_path}/common.sh
 conf_path=/home/centos/roboshop-shell
 
-echo congiguration path is ${conf_path}
 
-exit
 echo -e "\e[32m>>>>>>>>>>>>>> download the repo file  <<<<<<<<<<<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
@@ -28,7 +26,7 @@ echo -e "\e[32m>>>>>>>>>>>>>> installing the dependencies  <<<<<<<<<<<<<<<<<<<\e
 npm install
 
 echo -e "\e[32m>>>>>>>>>>>>>> copying the configuration file to systemd <<<<<<<<<<<<<<<<<<<\e[0m"
-cp  ${script_path}/cart.service /etc/systemd/system/cart.service
+cp  ${conf_path}/cart.service /etc/systemd/system/cart.service
 
 echo -e "\e[32m>>>>>>>>>>>>>> reloading the schema <<<<<<<<<<<<<<<<<<<\e[0m"
 systemctl daemon-reload
