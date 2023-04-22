bb86292b6f771621e1100dee69a4c7538f8e8926
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2023-04-22 19:11:09 +0530

    source common code

--- common.sh
+++ common.sh
@@ -9,9 +9,9 @@ func_print_head(){
 func_stat_check(){
         if [ "$1" -eq 0 ]
               then
-                echo -e "\e[32m Instalation Success\e[0m"
+                echo -e "\e[32m SUCCESS \e[0m"
               else
-                echo -e "\e[31m Installation Failure\e[0m"
+                echo -e "\e[31m  FAILED \e[0m"
                 exit 1
          fi
 }
