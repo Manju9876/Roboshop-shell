c74acf96ecabab859bf2134559c7506b18490be1
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2023-04-23 02:16:23 +0530

    source common code

--- mongodb.sh
+++ mongodb.sh
@@ -12,7 +12,7 @@ service_start="mongodb"
 
 func_print_head "updating the mongod.conf file to 0.0.0.0"
    sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf &>>${log_file}
-   func_stat_check
+   func_stat_check $?
 
 func_print_head "start service of mongodb"
 systemctl enable mongod &>>${log_file}
